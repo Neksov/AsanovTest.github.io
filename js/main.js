84cd464cb2faa4849b8555a6d0b3c585d8a495e8
Neksov <n2v@list.ru>
2020-08-29 17:06:08 +0300

    add card

--- js/main.js
+++ js/main.js
@@ -8,5 +8,28 @@ $(document).ready(function () {
   $(".nav__btn").click(function (event) {
     $(".nav__btn").toggleClass("active");
   });
+  $(".search__input").click(function (event) {
+    $(".search__input").toggleClass("active");
+  });
 
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      }
+    }
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
 });
